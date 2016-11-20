2c2c51c87cf2b59314cf02cc899bf4a7bd73769b
Joakim Loxdal <loxdalen@gmail.com>
2016-11-20 13:11:38 +0100

    Verifying form

--- app.config.js
+++ app.config.js
@@ -1,6 +1,24 @@
 var app = angular.module('alkokollen', ['ngRoute']);
 
-app.controller('mainCtrl', function($scope){
-	$scope.drinks = {}
+
+function welcomeController($scope){
+	$scope.person = {};
+	$scope.addPerson = function(){
+	}
+}
+
+app.controller('appCtrl', function($scope){
+	$scope.drinks = []
 	$scope.user = {}
-})
+
+	$scope.addDrink = function(){
+		console.log('adding drink');
+		$scope.drinks[$scope.drinks.length] = {
+			amount: $scope.amount,
+			percentage: $scope.percentage,
+			time: $scope.time,
+		}
+
+
+	}
+});
